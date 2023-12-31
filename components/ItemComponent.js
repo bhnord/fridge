dd2c152494c4a17365b8ef89ba191fc4e06f45c2
bhnord <b.nordemann@aol.com>
2023-12-30 20:41:32 -0500

    css

--- components/ItemComponent.js
+++ components/ItemComponent.js
@@ -30,6 +30,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    marginBottom: 10,
+    borderRadius: 5
   },
   item: {
     alignItems: "center",
